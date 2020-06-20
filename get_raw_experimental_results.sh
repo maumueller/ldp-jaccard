801c9155e345b4bec23de011ae29cf594c7d5bc7
Martin Aumüller <maau@itu.dk>
2020-06-20 23:52:40 +0200

    Some fixes.

--- get_raw_experimental_results.sh
+++ get_raw_experimental_results.sh
@@ -1,2 +1,2 @@
-wget http://itu.dk/people/maau/ldp-jaccard/raw_csv_ldp_jaccard.tar.gz
-tar xf raw_csv_ldp_jaccard.tar.gz
+wget http://itu.dk/people/maau/ldp-jaccard/results_sisap.tar.gz
+tar xf results_sisap.tar.gz
